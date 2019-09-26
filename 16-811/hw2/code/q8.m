efa252b0ae84ca982f50b47b8f199b98681739b2
Emma Benjaminson <ebenjami@andrew.cmu.edu>
2019-09-26 00:47:03 -0400

    finish pset 2 for 811

--- 16-811/hw2/code/q8.m
+++ 16-811/hw2/code/q8.m
@@ -27,21 +27,93 @@ data = importdata('paths.txt') ;
 %% Algorithm
 
 % take a starting location for t = 0 
-x0 = 1 ; 
-y0 = 1 ; 
-p0 = [x0,y0] ; 
+% p0 = [0.8, 1.8] ; 
+% p0 = [2.2, 1.0] ; 
+p0 = [2.7, 1.4] ; 
 
 % pick 3 paths such that the given starting point lies inside a triangle formed
 % by the starting points of these 3 paths
 
-% build a new path p by performing a weighted sum of the 3 paths. The
-% weights should be constant with time. P(0) should just be the given
-% starting point. 
+% the 3 paths be within a distance eps of each other
+eps = 3 ; 
 
 % indices of the chosen paths (+1 for y-coordinates)
 i = 1 ; 
 j = 3 ; 
 k = 5 ; 
+
+% build x array and y array
+xarray = [data(i,1), data(j,1), data(k,1)] ; 
+yarray = [data(i+1,1), data(j+1,1), data(k+1,1)] ; 
+
+% check that the starting points of the 3 paths are within eps distance of each other
+xcheck = max(xarray) - min(xarray) ; 
+ycheck = max(yarray) - min(yarray) ; 
+
+% check that all 3 paths are either above or below the ring of fire
+y1 = interp1(data(i,:),data(i+1,:),5) ; 
+y2 = interp1(data(j,:),data(j+1,:),5) ; 
+y3 = interp1(data(k,:),data(k+1,:),5) ; 
+
+% p(t) should never enter the ring of fire
+if (y1 > 7)  && (y2 > 7) && (y3 > 7)
+    circleCheck = 1 ; 
+elseif (y1 < 3) && (y2 < 3) && (y3 < 3)
+    circleCheck = 1 ; 
+else 
+    circleCheck = 0 ; 
+end
+
+result = triangleCheck(xarray, yarray, p0) ;  
+
+m = 1 ; 
+
+% sweep through all points 
+it = 1:2:97 ; 
+jt = 1:2:97 ; 
+kt = 1:2:97 ; 
+
+[I,J,K] = meshgrid(it,jt,kt) ; 
+I = reshape(I,[117649,1]) ; 
+J = reshape(J,[117649,1]) ; 
+K = reshape(K,[117649,1]) ; 
+
+while (result == 0) || (xcheck > eps) || (ycheck > eps) || (circleCheck == 0)
+        
+    i = I(m) ; 
+    j = J(m) ; 
+    k = K(m) ; 
+    
+    % build x array and y array
+    xarray = [data(i,1), data(j,1), data(k,1)] ; 
+    yarray = [data(i+1,1), data(j+1,1), data(k+1,1)] ; 
+
+    % check that the points are within eps distance of each other
+    xcheck = max(xarray) - min(xarray) ; 
+    ycheck = max(yarray) - min(yarray) ; 
+    
+    % check that all 3 paths are either above or below the ring of fire
+    y1 = interp1(data(i,:),data(i+1,:),5) ; 
+    y2 = interp1(data(j,:),data(j+1,:),5) ; 
+    y3 = interp1(data(k,:),data(k+1,:),5) ; 
+
+    % p(t) should never enter the ring of fire
+    if (y1 > 7)  && (y2 > 7) && (y3 > 7)
+        circleCheck = 1 ; 
+    elseif (y1 < 3) && (y2 < 3) && (y3 < 3)
+        circleCheck = 1 ; 
+    else 
+        circleCheck = 0 ; 
+    end
+
+    result = triangleCheck(xarray, yarray, p0) ; 
+    
+    m = m + 1 ; 
+end
+
+% build a new path p by performing a weighted sum of the 3 paths. The
+% weights should be constant with time. P(0) should just be the given
+% starting point. 
 
 % weights for each of the chosen paths
 a1 = 0.5 ; 
@@ -49,8 +121,8 @@ a2 = 0.25 ;
 a3 = 0.25 ; 
 
 % calculated path
-px = [x0, a1*data(i,2:end) + a2*data(j,2:end) + a3*data(k,2:end)] ; 
-py = [y0, a1*data(i+1,2:end) + a2*data(j+1,2:end) + a3*data(k+1,2:end)] ; 
+px = [p0(1), a1*data(i,2:end) + a2*data(j,2:end) + a3*data(k,2:end)] ; 
+py = [p0(2), a1*data(i+1,2:end) + a2*data(j+1,2:end) + a3*data(k+1,2:end)] ; 
 
 % use interpolation to find the values for p(t) at some given time scale
 % (we will need to justify why we picked the time scale and the
@@ -63,7 +135,6 @@ t = 1:dt:50 ;
 px_interp = interp1(tbaseline,px,t) ; 
 py_interp = interp1(tbaseline,py,t) ; 
 
-% p(t) should never enter the ring of fire
 
 %% Demonstration
 
@@ -96,86 +167,33 @@ hold on
 plot(px_interp,py_interp,'-g') ; 
 hold on 
 xlabel('X') ; ylabel('Y') ; 
-legend('Ring of Fire','Path 1','Path 2','Path 3','Interpolated Path','Location','southeast') ; 
+legend('Ring of Fire','Path 1','Path 2','Path 3','Interpolated Path','Location','northwest') ; 
 xlim([0 12]) ; ylim([0 12]) ; 
+title('Starting Point (2.7, 1.4)') ; 
+axis equal
 
 %% Functions
-
-% divided difference function
-function pn = DivDiff(x,fx,xdes)
-
-    % get number of sample points
-    n = length(x) ; 
-    ncheck = length(fx) ; 
-    
-    if n ~= ncheck
-        f = msgbox("Sample point arrays x and fx do not have the same length.","Error") ; 
-        return ; 
-    end
-    
-    % calculate b
-    % the structure of b is: 
-    % b = [fx ,  0 ,   0 ,   0 ...
-    %      fx2 , b11 , 0 ,   0 ...
-    %      fx3 , b12 , b21 , 0 ...
-    %      fx4 , b13 , b22 , b31 ...]
-    % b = j rows x i columns
-    
-    % initialize b
-    b = zeros(n,n) ; 
-    
-    % first column of b uses fx values
-    for j = 1:n
-            b(j,1) = fx(j) ; 
-    end
-    
-    % columns 2:n are calculated values of b
-    for i = 2:n
-        for j = (1 + (i-1)):n 
-            b(j,i) = ( b(j,(i-1)) - b((j-1),(i-1)) ) / ( x(j) - x(j-1) ) ; 
-        end
-    end        
-    
-    % find starting x values
-    % search for xlow which is less than xdes and xhi which is greater than
-    % xdes
-    % xlow and xhi should bracket xdes
-    % save the indices of xlow and xhi, these should be used to build the
-    % polynomial starting with index of xlow, and expanding up to larger
-    % values
-    for j = 1:(n-1)
-        xlow = x(j) ; 
-        jlow = j ; 
-        xhigh = x(j+1) ; 
-        
-        if xlow < xdes && xhigh > xdes
-            break
-        end
-    end
-    
-    % build polynomial px
-    % solve px for xdes
-    px = b(jlow,1) ; 
-    xmultiplier = 1 ; 
-    
-    j = jlow ; % start looping through jlow-th row and go down
-    i = 2 ; 
-    
-    while j <= n && i <= n 
-        xmultiplier = xmultiplier * (xdes - x(j)) ; % calculate (x - xi)
-        px = px + b(j,i)*xmultiplier ;   % add new term to polynomial
-        j = j + 1 ; % increase row index by 1 
-        i = i + 1 ; % increase column index by 1
-
-    end
-    
-    pn = px ; 
-
-end
 
 % plot a circle
 function [xc,yc] = circle(x,y,r)
     theta = 0:pi/50:2*pi ; 
     xc = r*cos(theta) + x ; 
     yc = r*sin(theta) + y ; 
+end
+
+% check if a point is inside a triangle or outside
+function result = triangleCheck(xarray, yarray, p0)
+    A = [xarray ; 
+         yarray ; 
+         ones(1,3) ] ; 
+     
+    b = [p0, 1] ; 
+    
+    v = inv(A)*b' ; 
+    
+    if (sum(v>0) == 3) && (sum(v) == 1)
+        result = 1 ; 
+    else 
+        result = 0 ; 
+    end
 end
